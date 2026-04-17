1424717f5764d8083e2ec26d3ffc12cc0cba6a5c
fputra-source <frian.putra@gmail.com>
2026-04-17 15:22:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  // Caching standar agar tombol install muncul
+self.addEventListener('activate', function(e) {
+  return self.clients.claim();
+});
+
+// Fetch listener ini WAJIB ada agar Chrome menganggap PWA ini sah
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request).catch(function() {
+      return new Response('Offline');
+  }));
 });
